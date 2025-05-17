937800c2e8b803a31e7c514e796f3368a501e41d
Mar972310 <maria.torres-m@mail.escuelaing.edu.co>
2025-05-17 06:13:21 -0500

    sdfadfs

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig(({ mode }) => {
+  // Carga las variables del .env según el modo (development o production)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      'process.env': env, // Si necesitas acceso tipo process.env
+    },
+    server: {
+      port: 5173,
+    }
+  };
+});
