aae99060161865f5f530d7ae82ae97af5707c410
Mar972310 <maria.torres-m@mail.escuelaing.edu.co>
2025-05-17 06:12:08 -0500

    sdf

--- src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useIsAuthenticated } from "@azure/msal-react";
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children }) => {
-    const isAuthenticated = useIsAuthenticated();
-    return isAuthenticated ? children : <Navigate to="/" />;
-};
-
-export default PrivateRoute;
--- src/components/Profile.jsx
+++
@@ -1,9 +0,0 @@
-const Profile = () => {
-    return (
-      <div className="profile-container">
-        <img src="/assets/default-avatar.png" alt="Avatar por defecto" className="profile-image" />
-      </div>
-    );
-  };
-
-export default Profile;
